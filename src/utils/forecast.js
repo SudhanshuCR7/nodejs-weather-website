dbce020883539e63d5a906396bbd22f5e0766992
SudhanshuCR7 <sudhanshu17cr7@gmail.com>
2022-05-19 00:33:39 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('postman-request')
+
+const forecast = (x, y, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=cde903f4ad0039bd2c3172e51e55985b&query=' + x + ',' + y 
+
+    request({url:url , json:true} , (error, response) =>{
+        if(error) {
+            callback('Unable to connect !', undefined)
+        }
+        else if(response.body.error){
+            callback('Unable to fetch data', undefined)
+        }
+        else{
+        
+            callback(undefined,"It is currently " + (response.body.current.temperature) + " degreees out. It feels like " + (response.body.current.feelslike) + " degress out.")
+        }
+    })
+}
+
+module.exports= forecast
